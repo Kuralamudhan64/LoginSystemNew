0f6ea50e603014d970734019ab59d6826e0ed3c7
Kuralamudhan64 <108523481+Kuralamudhan64@users.noreply.github.com>
2025-08-20 15:51:45 +0530

    Add files via upload

---
+++ LoginSystemUI/wwwroot/js/Home/login.js
@@ -0,0 +1,41 @@
+﻿console.log("Hello");
+
+
+let button = document.getElementById("button");
+
+
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let password = document.getElementById("password").value;
+    console.log({ name ,password});
+    
+    const url = "https://localhost:7087/api/Home/SignUp";
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: name,
+            password: password
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.isSuccessfull)
+                alert(data.message);
+            else
+                alert("Error", data.message);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+})
+
+
+
+
+
