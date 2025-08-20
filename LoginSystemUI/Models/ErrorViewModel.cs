3ea73d258a65041c16ed9fd5002e642168692324
Kuralamudhan64 <108523481+Kuralamudhan64@users.noreply.github.com>
2025-08-20 15:49:20 +0530

    Add files via upload

---
+++ LoginSystemUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginSystemUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
